00dc7fd0557ca371abf92b10cd823b7c9199e1fb
AndriiVukolov <andrii.vukolov@alnicko.com>
2025-05-02 17:03:23 +0300

    Uart functional is added. (#2)
    * ADD: comment to new exludes in .gitignore
    
    * REMOVE: Debug/ folder
    
    * ADD: UART transmission Hello World! every second by the interrupt of timer2
    
    * ADD: buffer and uart send msg in main cicle with delay
    
    * REPLACE: while... to strlen

---
+++ Core/Inc/loging.h
@@ -0,0 +1,4 @@
+
+
+UART_HandleTypeDef * UartInit(int type);
+void UartSendString(const unsigned char * phrase, UART_HandleTypeDef* huart);
---
+++ Core/Src/loging.c
@@ -0,0 +1,39 @@
+#include "usart.h"
+#include "string.h"
+
+//UART_HandleTypeDef huart4;
+//UART_HandleTypeDef huart1;
+
+UART_HandleTypeDef * UartInit(int type)
+{
+	UART_HandleTypeDef* uartHandle;
+
+
+	if (type == 4)
+	{
+		uartHandle = &huart4;
+		MX_UART4_Init();
+	}
+	else if (type == 1)
+	{
+		uartHandle = &huart1;
+		MX_USART1_UART_Init();
+	}
+
+
+
+
+	return (uartHandle);
+}
+
+void UartSendString(unsigned char * phrase, UART_HandleTypeDef* huart)
+{
+	int qty = 0;
+
+	qty = strlen(phrase);
+	if (HAL_UART_Transmit(huart, phrase, qty, 1000) != HAL_OK)
+	  {
+			Error_Handler();
+	  }
+
+}
